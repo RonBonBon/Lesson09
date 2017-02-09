7dcd1c23cd327d9e416d931e01ca427fcd2ccf2c
hackerudroids <korsjava@gmail.com>
2017-02-09 20:13:53 +0200

    Ver 01.01

--- src/com/arichafamily/java/Employee.java
+++ src/com/arichafamily/java/Employee.java
@@ -13,31 +13,18 @@ public class Employee {
     private String employeeID;
     private String socialID;
     private String department;
-
     private String phone;
 
-    private Date hireDate;
-    private String company;
-    private double salary;
-
-    public Employee() {
-        this.firstName = getString("Enter First name");
-        this.lastName = getString("Enter Last name");
-        this.salary = getDouble("Enter Salary");
-        this.empID = getInt("Enter Employee ID");
-        this.department = setString("Enter Department name");
-        this.company = ("Enter Company name");
+    public Employee(String firstName, String lastName, String employeeID, String socialID, String department, String phone) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.employeeID = employeeID;
+        this.socialID = socialID;
+        this.department = department;
+        this.phone = phone;
     }
 
-    public Employee(String firstName, String lastName, double salary, int empID, String department, String company) {
-    }
+    public static String getString(){
 
-    public void showEmployee(){
-        System.out.println("First name: " + firstName);
-        System.out.println("Last name: " + lastName);
-        System.out.println("Your Salary: " + salary);
-        System.out.println("Employee ID: " + empID);
-        System.out.println("Department: " + department);
-        System.out.println("Company: " + company);
     }
 }
--- src/com/arichafamily/java/Main.java
+++ src/com/arichafamily/java/Main.java
@@ -6,10 +6,5 @@ public class Main {
 	// write your code here
         Employee e = new Employee();
         e.showEmployee();
-
-        System.out.println();
-
-        ("Ora", "Aricha", 10000.45, 60789, "Finance", "Cellcom");
-        e.showEmployee();
     }
 }
